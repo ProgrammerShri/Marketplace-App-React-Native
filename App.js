7d0f607b541bd3d12477528236e1d8cc335124a2
ProgrammerShri <shrikantdewangan3@gmail.com>
2022-06-11 13:00:02 +0530

    Input Component Created

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import Screen from "./app/components/Screen";
@@ -8,7 +10,12 @@ import ListingsScreen from "./app/screens/ListingsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+  return (
+    <Screen>
+      <AppTextInput icon="account-circle" placeholder="First Name" />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
