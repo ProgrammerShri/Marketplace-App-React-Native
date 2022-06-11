758194bb2f8787f47eb927b63999798e3043f178
ProgrammerShri <shrikantdewangan3@gmail.com>
2022-06-11 12:13:26 +0530

    Icon Component Created

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StyleSheet, View } from "react-native";
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="#f00" iconColor="white" />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
---
+++ app/components/Icon.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import colors from "../config/colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const Icon = ({
+  name,
+  size = 40,
+  backgroundColor = colors.black,
+  iconColor = colors.white,
+}) => {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size,
+        backgroundColor,
+        borderRadius: size / 2,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <MaterialCommunityIcons name={name} size={size * 0.5} color={iconColor} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Icon;
