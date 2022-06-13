88c1107650370a5e1a39a166b4001b3e5c9ef516
ProgrammerShri <shrikantdewangan3@gmail.com>
2022-06-13 20:22:36 +0530

    some minor changes

--- Frontend/App.js
+++ Frontend/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
+      {/* <AuthNavigator /> */}
     </NavigationContainer>
   );
 }
